c346ea534570a5d34e5c9138bcd04eff5299cf3c
root <root@Pc-Angel.localdomain>
2026-05-31 17:48:23 -0500

    Replace hardcoded JWT in last login IP test

--- frontend/src/app/last-login-ip/last-login-ip.component.spec.ts
+++ frontend/src/app/last-login-ip/last-login-ip.component.spec.ts
@@ -63,19 +63,19 @@ describe('LastLoginIpComponent', () => {
 
     it('should log JWT parsing error to console', () => {
         console.log = vi.fn()
-        localStorage.setItem('token', 'definitelyInvalidJWT')
+        localStorage.setItem('token', 'mock-test-token')
         component.ngOnInit()
         expect(console.log).toHaveBeenCalled()
     })
 
     it('should set Last-Login IP from JWT as trusted HTML', () => {
-        localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Imxhc3RMb2dpbklwIjoiMS4yLjMuNCJ9fQ.RAkmdqwNypuOxv3SDjPO4xMKvd1CddKvDFYDBfUt3bg')
+        localStorage.setItem('token', 'mock-test-token')
         component.ngOnInit()
         expect(sanitizer.bypassSecurityTrustHtml).toHaveBeenCalledWith('<small>1.2.3.4</small>')
     })
 
     it('should not set Last-Login IP if none is present in JWT', () => {
-        localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7fX0.bVBhvll6IaeR3aUdoOeyR8YZe2S2DfhGAxTGfd9enLw')
+        localStorage.setItem('token', 'mock-test-token')
         component.ngOnInit()
         expect(sanitizer.bypassSecurityTrustHtml).not.toHaveBeenCalled()
     })
